5293d124ceacda0bed556b9387044aedb83ab389
Maciej Krüger <mkg20001@gmail.com>
2019-04-14 16:20:41 +0200

    feat: add the sysadmin tools

---
+++ .mods.d/3-tools/sysadmin-tools.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# htop: process monitor
+sudo apt install -y htop
+# nload/nethogs: network monitor
+sudo apt install -y nload nethogs
+# iotop: disk monitor
+sudo apt install -y iotop
+# wget/curl: HTTP
+sudo apt install -y wget curl
+# nano: editor
+sudo apt install -y nano
+# tree/ncdu: file analytics
+sudo apt install -y tree ncdu
+# glances: netdata for cli, basically
+sudo apt install -y glances
+
