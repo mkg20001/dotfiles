9aab97cbce5686e71e1221c3826c03ecde0864ed
Maciej Krüger <mkg20001@gmail.com>
2019-04-14 17:39:58 +0200

    feat: add more stuff

---
+++ .mods.d/1-basic/netdata.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# this has to be first, so we have build tools installed for cjdns
+sudo bash <(curl -Ss https://my-netdata.io/kickstart.sh) --dont-wait
--- .mods.d/3-tools/nodejs.sh
+++ .mods.d/3-tools/nodejs.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-# TODO: check if this works
-curl https://deb.nodesource.com/setup_10x | sudo bash -
-sudo apt update
-sudo apt install node -y
+wget --quiet -O - https://deb.nodesource.com/gpgkey/nodesource.gpg.key | apt-key add -
+VERSION=node_10.x
+DISTRO="$(lsb_release -s -c)"
+echo "deb https://deb.nodesource.com/$VERSION $DISTRO main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+echo "deb-src https://deb.nodesource.com/$VERSION $DISTRO main" | sudo tee -a /etc/apt/sources.list.d/nodesource.list
+sudo apt-get update
+sudo apt install -y nodejs
