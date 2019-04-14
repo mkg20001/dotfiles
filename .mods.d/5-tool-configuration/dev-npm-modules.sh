9e0698a8649538d1c4cebef8c18ad2d972cf4740
Maciej Krüger <mkg20001@gmail.com>
2019-04-14 16:58:18 +0200

    feat: add ipfs and more npm stuff

--- .mods.d/5-tool-configuration/dev-npm-modules.sh
+++ .mods.d/5-tool-configuration/dev-npm-modules.sh
@@ -4,5 +4,5 @@
 
 npm i -g aegir # this one is for all the protocol-labs projects
 npm i -g nodemon parcel http-server # watchers, web dev, etc
-npm i -g pino-pretty # show pretty logs
-
+npm i -g pino-pretty  # show pretty logs
+npm i -g npm-check-updates google-font-installer wscat json5 pkg # other
