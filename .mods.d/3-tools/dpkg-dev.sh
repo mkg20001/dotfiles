d99093555a9ed64dda003ccb002b278bd35ef9b4
Maciej Krüger <mkg20001@gmail.com>
2019-04-14 18:29:22 +0200

    fix: nodejs

---
+++ .mods.d/3-tools/dpkg-dev.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo apt install -y debconf-utils dpkg-dev
+# installs debconf-get-selections which can automate dpkg package config prompts
--- .mods.d/3-tools/nodejs.sh
+++ .mods.d/3-tools/nodejs.sh
@@ -7,3 +7,7 @@ echo "deb https://deb.nodesource.com/$VERSION $DISTRO main" | sudo tee /etc/apt/
 echo "deb-src https://deb.nodesource.com/$VERSION $DISTRO main" | sudo tee -a /etc/apt/sources.list.d/nodesource.list
 sudo apt-get update
 sudo apt install -y nodejs
+
+# there is literally no way arround this, except for a bunch of sudo+env hacks
+sudo chown "$(whoami):$(whoami)" /usr/lib/node_modules
+sudo chown "$(whoami):$(whoami)" /usr/bin
