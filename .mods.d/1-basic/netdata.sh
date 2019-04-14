93e6a98b0465dfe7cfafd3886b3525df55fa6ded
Maciej Krüger <mkg20001@gmail.com>
2019-04-14 17:59:11 +0200

    fix: netdata

--- .mods.d/1-basic/netdata.sh
+++ .mods.d/1-basic/netdata.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # this has to be first, so we have build tools installed for cjdns
-sudo sh -c 'bash <(curl -Ss https://my-netdata.io/kickstart.sh) --dont-wait'
+sudo bash -c 'bash <(curl -Ss https://my-netdata.io/kickstart.sh) --dont-wait'
