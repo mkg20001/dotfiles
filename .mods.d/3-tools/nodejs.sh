adc1bafe20ebdcfb7d1cbe199c985883bbf90174
Maciej Krüger <mkg20001@gmail.com>
2019-04-14 21:36:18 +0200

    fix: missing sudo

--- .mods.d/3-tools/nodejs.sh
+++ .mods.d/3-tools/nodejs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget --quiet -O - https://deb.nodesource.com/gpgkey/nodesource.gpg.key | apt-key add -
+wget --quiet -O - https://deb.nodesource.com/gpgkey/nodesource.gpg.key | sudo apt-key add -
 VERSION=node_10.x
 DISTRO="$(lsb_release -s -c)"
 echo "deb https://deb.nodesource.com/$VERSION $DISTRO main" | sudo tee /etc/apt/sources.list.d/nodesource.list
