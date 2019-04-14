c5447d84e2feff563eb44ff20ac8ff3fbb357fad
Maciej Krüger <mkg20001@gmail.com>
2019-04-14 17:26:03 +0200

    feat: add ci

--- .mods.d/2-net/cjdns.sh
+++ .mods.d/2-net/cjdns.sh
@@ -3,36 +3,32 @@
 # rerun: upgrade
 # version: v20.2
 
-    CJD_VER="v20.2"
-    cjdMain() {
-      cd /opt
-      if [ -e cjdns ]; then
-        cd cjdns
-        git fetch -p
-        git reset --hard HEAD
-        git checkout cjdns-$CJD_VER
-      else
-        git clone https://github.com/cjdelisle/cjdns.git -b cjdns-$CJD_VER
-        cd cjdns
-      fi
+CJD_VER="v20.2"
+cd /opt
+if [ -e cjdns ]; then
+  cd cjdns
+  sudo git fetch -p
+  sudo git reset --hard HEAD
+  sudo git checkout cjdns-$CJD_VER
+else
+  sudo git clone https://github.com/cjdelisle/cjdns.git -b cjdns-$CJD_VER
+  cd cjdns
+fi
 
-      git clean -dxf
-      # patch for test-runner timeout
-      echo ZGlmZiAtLWdpdCBhL25vZGVfYnVpbGQvVGVzdFJ1bm5lci5qcyBiL25vZGVfYnVpbGQvVGVzdFJ1bm5lci5qcwppbmRleCA4N2FjZDM4MC4uODA0NDE0NzcgMTAwNjQ0Ci0tLSBhL25vZGVfYnVpbGQvVGVzdFJ1bm5lci5qcworKysgYi9ub2RlX2J1aWxkL1Rlc3RSdW5uZXIuanMKQEAgLTE1LDcgKzE1LDcgQEAgdmFyIFNwYXduID0gcmVxdWlyZSgnY2hpbGRfcHJvY2VzcycpLnNwYXduOwogdmFyIEh0dHAgPSByZXF1aXJlKCJodHRwIik7CiB2YXIgRnMgPSByZXF1aXJlKCJmcyIpOwogCi12YXIgVElNRU9VVCA9IDE1MDAwOwordmFyIFRJTUVPVVQgPSA2MDAwMDA7CiAKIHZhciBwYXJzZVVSTCA9IGZ1bmN0aW9uICh1cmwpCiB7Cg== | base64 -d | git apply -
-      # TODO remove patch above once https://github.com/cjdelisle/cjdns/pull/1178/files gets released
-      TestRunner_TIMEOUT=600000 ./do
-      rm -f /usr/bin/cjdroute
-      cp -p /opt/cjdns/cjdroute /usr/bin
-      [ ! -e /etc/cjdroute.conf ] && sudo bash -c '(umask 077 && ./cjdroute --genconf > /etc/cjdroute.conf)'
+sudo git clean -dxf
+# patch for test-runner timeout
+echo ZGlmZiAtLWdpdCBhL25vZGVfYnVpbGQvVGVzdFJ1bm5lci5qcyBiL25vZGVfYnVpbGQvVGVzdFJ1bm5lci5qcwppbmRleCA4N2FjZDM4MC4uODA0NDE0NzcgMTAwNjQ0Ci0tLSBhL25vZGVfYnVpbGQvVGVzdFJ1bm5lci5qcworKysgYi9ub2RlX2J1aWxkL1Rlc3RSdW5uZXIuanMKQEAgLTE1LDcgKzE1LDcgQEAgdmFyIFNwYXduID0gcmVxdWlyZSgnY2hpbGRfcHJvY2VzcycpLnNwYXduOwogdmFyIEh0dHAgPSByZXF1aXJlKCJodHRwIik7CiB2YXIgRnMgPSByZXF1aXJlKCJmcyIpOwogCi12YXIgVElNRU9VVCA9IDE1MDAwOwordmFyIFRJTUVPVVQgPSA2MDAwMDA7CiAKIHZhciBwYXJzZVVSTCA9IGZ1bmN0aW9uICh1cmwpCiB7Cg== | base64 -d | sudo git apply -
+# TODO remove patch above once https://github.com/cjdelisle/cjdns/pull/1178/files gets released
+sudo sh -c "TestRunner_TIMEOUT=600000 ./do"
+sudo rm -f /usr/bin/cjdroute
+sudo cp -p /opt/cjdns/cjdroute /usr/bin
+[ ! -e /etc/cjdroute.conf ] && sudo bash -c '(umask 077 && ./cjdroute --genconf > /etc/cjdroute.conf)'
 
-      port=$(sudo cat /etc/cjdroute.conf | grep "bind" | grep "0.0.0.0" | grep "[0-9][0-9][0-9]*" -o)
-      ufw allow "$port/udp" comment CJDNS
+port=$(sudo cat /etc/cjdroute.conf | grep "bind" | grep "0.0.0.0" | grep "[0-9][0-9][0-9]*" -o)
+sudo ufw allow "$port/udp" comment CJDNS
 
-      cp contrib/systemd/cjdns.service /etc/systemd/system/
-      cp contrib/systemd/cjdns-resume.service /etc/systemd/system/
+sudo cp contrib/systemd/cjdns.service /etc/systemd/system/
+sudo cp contrib/systemd/cjdns-resume.service /etc/systemd/system/
 
-      systemctl enable cjdns
-      systemctl restart cjdns
-    }
-
-cjdMain
+sudo systemctl enable cjdns
+sudo systemctl restart cjdns
