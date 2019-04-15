c2df826315230559056ba21464ed5b8552337e86
Maciej Krüger <mkg20001@gmail.com>
2019-04-15 14:06:26 +0200

    feat: add ts3 and 2fa script

---
+++ .mods.d/4-apps/ts3.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+DL=$(curl -s https://teamspeak.com/en/downloads/ | grep -o "https.*run" | grep amd64 | uniq)
+wget -O /tmp/ts3.run
+sed "s|^MS_PrintLicense$|true|g" -i /tmp/ts3.run 
+chmod +x /tmp/ts3.run
+/tmp/ts3.run --target "$HOME/.ts3"
+rm /tmp/ts3.run
