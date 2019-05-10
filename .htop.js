66c7f016b79524d12b7d9cd6a3126915f2301517
Maciej Krüger <mkg20001@gmail.com>
2019-05-10 09:49:33 +0200

    feat: add .htop.js

---
+++ .htop.js
@@ -0,0 +1,8 @@
+'use strict'
+
+const exec = require('child_process').execSync
+const split = Buffer.from('0D1B5B3864', 'hex')
+let out = exec("htop << f\nq\nf").toString() // run htop for a sec
+out = out.substr(80).split(String(split))[0].split(' PID USER ')[0]
+console.log(out.substr(0, out.length - 3))
+// console.log("\n\n\n\n\n\n") //add line breaks (because reset)
