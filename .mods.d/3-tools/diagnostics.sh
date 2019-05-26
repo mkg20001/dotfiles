6bbd41a087c227299a0d5a20fa127bcca1a363f5
Maciej Krüger <mkg20001@gmail.com>
2019-05-26 16:03:47 +0200

    fix: lstopo cmd is in hwloc pkg

--- .mods.d/3-tools/diagnostics.sh
+++ .mods.d/3-tools/diagnostics.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt install -y inxi lstopo
+sudo apt install -y inxi hwloc
 
 # inxi -v7: List ALL the specs INCLUDING software
 # lstopo: show a nice picture of the hardware configuration
