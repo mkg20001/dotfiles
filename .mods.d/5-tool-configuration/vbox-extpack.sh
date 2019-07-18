83899b2b14356c478dc6b51e28798517bd149d40
Maciej Krüger <mkg20001@gmail.com>
2019-07-18 16:20:39 +0200

    feat: add vbox extpack and ndb/0x

---
+++ .mods.d/5-tool-configuration/vbox-extpack.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# rerun: change watch
+# watch-file: /usr/bin/VBoxManage
+
+VERSION=$(VBoxManage --version | tail -1 | cut -f 1 -d "r")
+curl -Lo /tmp/Oracle_VM_VirtualBox_Extension_Pack-${VERSION}.vbox-extpack http://download.virtualbox.org/virtualbox/${VERSION}/Oracle_VM_VirtualBox_Extension_Pack-${VERSION}.vbox-extpack
+yes | sudo VBoxManage extpack install /tmp/Oracle_VM_VirtualBox_Extension_Pack-${VERSION}.vbox-extpack
+rm -rf /tmp/Oracle_VM_VirtualBox_Extension_Pack-${VERSION}.vbox-extpack
+
