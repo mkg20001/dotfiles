667dbf06d8caea3a7bfaf5deeeb55668efcaa706
Maciej Krüger <mkg20001@gmail.com>
2019-07-21 23:50:03 +0200

    feat: add jay-repl and fixup ignores

--- .mods.d/5-tool-configuration/dev-npm-modules.sh
+++ .mods.d/5-tool-configuration/dev-npm-modules.sh
@@ -7,3 +7,4 @@ npm i -g nodemon parcel http-server # watchers, web dev, etc
 npm i -g pino-pretty  # show pretty logs
 npm i -g npm-check-updates google-font-installer wscat json5 pkg # other
 npm i -g ndb 0x # debugger and profiler
+npm i -g jay-repl # better repl
