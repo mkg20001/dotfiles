74b9057926b8139ca231326233a0f592e4d9c1b7
Maciej Krüger <mkg20001@gmail.com>
2019-07-24 13:54:03 +0200

    feat: add replace flag to vbox extpack

--- .mods.d/5-tool-configuration/vbox-extpack.sh
+++ .mods.d/5-tool-configuration/vbox-extpack.sh
@@ -5,6 +5,6 @@
 
 VERSION=$(VBoxManage --version | tail -1 | cut -f 1 -d "r")
 curl -Lo /tmp/Oracle_VM_VirtualBox_Extension_Pack-${VERSION}.vbox-extpack http://download.virtualbox.org/virtualbox/${VERSION}/Oracle_VM_VirtualBox_Extension_Pack-${VERSION}.vbox-extpack
-yes | sudo VBoxManage extpack install /tmp/Oracle_VM_VirtualBox_Extension_Pack-${VERSION}.vbox-extpack
+yes | sudo VBoxManage extpack install --replace /tmp/Oracle_VM_VirtualBox_Extension_Pack-${VERSION}.vbox-extpack
 rm -rf /tmp/Oracle_VM_VirtualBox_Extension_Pack-${VERSION}.vbox-extpack
 
