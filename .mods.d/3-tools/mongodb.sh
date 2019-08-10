cff7d9a1465c3324c580ff3590336338e5c6755b
Maciej Krüger <mkg20001@gmail.com>
2019-08-10 12:45:47 +0200

    feat: add cmd routine, split mongodb script

--- .mods.d/3-tools/mongodb.sh
+++ .mods.d/3-tools/mongodb.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# rerun: change watch
-# watch-file: /usr/bin/mongod
-# watching binary so we can re-disable it after upgrades
+# rerun: change
 
 sudo apt install mongodb -y
-sudo systemctl disable mongodb # takes too much mem
