4b28cd2f16cc5d4888a43ee0a30842a735280f94
Maciej Krüger <mkg20001@gmail.com>
2019-08-12 11:55:01 +0200

    feat: updates

--- .mods.d/5-tool-configuration/npm-global-madness.sh
+++ .mods.d/5-tool-configuration/npm-global-madness.sh
@@ -2,5 +2,6 @@
 
 # rerun: change
 
+sudo rm -vf /node_modules
 sudo ln -s /usr/lib/node_modules/ /node_modules # we didn't start the fire... :notes:
 
---
+++ .mods.d/5-tool-configuration/pyls.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# rerun: change upgrade
+
+sudo python -m pip install 'python-language-server[all]' --upgrade
