c96e17f726e57c7462f2fbc3fbef3c49d7a3f985
Maciej Krüger <mkg20001@gmail.com>
2019-08-14 01:01:15 +0200

    fix: tool conf

--- .mods.d/5-tool-configuration/psql-root.sh
+++ .mods.d/5-tool-configuration/psql-root.sh
@@ -3,4 +3,4 @@
 # rerun: watch
 
 me=$(whoami)
-sudo su postgres -c "psql -c 'CREATE ROLE $me; ALTER ROLE $me WITH login; ALTER ROLE $me WITH superuser;'"
+sudo su postgres -c "psql -c 'CREATE ROLE $me; ALTER ROLE $me WITH login; ALTER ROLE $me WITH superuser; ALTER ROLE $me WITH createdb; CREATE DATABASE $me;'"
