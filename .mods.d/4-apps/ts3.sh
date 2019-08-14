748c7667b3b0c03dc44ddc5fe984f91542aa7d50
Maciej Krüger <mkg20001@gmail.com>
2019-08-14 09:52:31 +0200

    fix: ts3 script spam

--- .mods.d/4-apps/ts3.sh
+++ .mods.d/4-apps/ts3.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+# rerun: never
+
 DL=$(curl -s https://teamspeak.com/en/downloads/ | grep -o "https.*run" | grep amd64 | uniq)
-wget -O /tmp/ts3.run "$DL"
+wget -O /tmp/ts3.run "$DL" --progress=dot:giga
 sed "s|^MS_PrintLicense$|true|g" -i /tmp/ts3.run 
 chmod +x /tmp/ts3.run
 /tmp/ts3.run --target "$HOME/.ts3"
