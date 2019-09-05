483ded613c9b5f5995340f18e1e84f7620dc2cd0
Maciej Krüger <mkg20001@gmail.com>
2019-09-05 15:52:01 +0200

    feat: adblock

---
+++ .mods.d/5-tool-configuration/npm-adblock.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# rerun: change watch
+# watch-file: /usr/lib/node_modules/npm/lib/install/action/postinstall.js
+
+npm i -g npm-adblock
+adblock-patch
--- .mods.d/5-tool-configuration/psql-root.sh
+++ .mods.d/5-tool-configuration/psql-root.sh
@@ -3,4 +3,5 @@
 # rerun: watch
 
 me=$(whoami)
-sudo su postgres -c "psql -c 'CREATE ROLE $me; ALTER ROLE $me WITH login; ALTER ROLE $me WITH superuser; ALTER ROLE $me WITH createdb; CREATE DATABASE $me;'"
+sudo su postgres -c "psql -c 'CREATE ROLE $me; ALTER ROLE $me WITH login; ALTER ROLE $me WITH superuser; ALTER ROLE $me WITH createdb;'"
+sudo su postgres -c "psql -c 'CREATE DATABASE $me;'"
