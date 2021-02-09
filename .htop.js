f9c6ffd6440be6a76b67c2fbf5f020e3ca0252df
Maciej Krüger <mkg20001@gmail.com>
2021-02-09 16:08:36 +0100

    fix: work with newer versions

--- .htop.js
+++ .htop.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const exec = require('child_process').execSync
-const split = Buffer.from('0D1B5B3864', 'hex')
+const split = Buffer.from('0D1B5B3964', 'hex')
 let out = exec("htop << f\nq\nf").toString() // run htop for a sec
 out = out.substr(80).split(String(split))[0].split(' PID USER ')[0]
 console.log(out.substr(0, out.length - 3))
